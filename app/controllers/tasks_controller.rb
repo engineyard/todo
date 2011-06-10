71928f0b3a6d927148e78266eb90e7a9e3c4e3ef
Britt Mileshosky <bmileshosky@engineyard.com>
2011-06-09 18:35:39 -0700

    Clean up controller, removed unneccesary code.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,86 +1,49 @@
 class TasksController < ApplicationController
-  # GET /tasks
-  # GET /tasks.xml
+
+
   def index
-    
     @todo = Task.where(:done => false)
     @task = Task.new
     @lists = List.all
     @list = List.new
     
     respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @tasks }
+      format.html
     end
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.xml
-  def show
-    @task = Task.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @task }
-    end
-  end
-
-  # GET /tasks/new
-  # GET /tasks/new.xml
-  def new
-    @task = Task.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @task }
-    end
-  end
-
-  # GET /tasks/1/edit
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  # POST /tasks
-  # POST /tasks.xml
   def create
     @task = Task.new(params[:task])
     respond_to do |format|
       if @task.save
         format.html { redirect_to(tasks_url(:list => @task.list.id), :notice => 'Task was successfully created.') }
-        format.xml  { render :xml => @task, :status => :created, :location => @task }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
       end
     end
   end
+  
 
-  # PUT /tasks/1
-  # PUT /tasks/1.xml
   def update
     @task = Task.find(params[:id])
 
     respond_to do |format|
       if @task.update_attributes(params[:task])
         format.html { redirect_to( tasks_url(:list => @task.list.id), :notice => 'Task was successfully updated.') }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.xml
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
 
     respond_to do |format|
       format.html { redirect_to(tasks_url(:list => @task.list_id)) }
-      format.xml  { head :ok }
     end
   end
   
