faf0a463aa19e185fc2e328f8567eef4b2a72a29
Chris Tosswill <ctosswill@engineyard.com>
2011-06-13 15:55:31 -0700

    made basic tasks

--- db/migrate/20110613204114_add_lists.rb
+++ db/migrate/20110613204114_add_lists.rb
@@ -1,8 +1,12 @@
 class AddLists < ActiveRecord::Migration
   def self.up
-    List.create(:name => 'Inbox')
-    list = List.create(:name => 'Getting Started')
-    list.tasks.create(:name => 'Cool task')
+  
+    list = List.create(:name => 'Welcome')
+    list.tasks.create(:name => 'Check out our docs http://docs.engineyard.com/ ')
+    list.tasks.create(:name => 'Follow @EngineYard http://twitter.com/#!/engineyard')
+    list.tasks.create(:name => 'Follow @eycloud http://twitter.com/#!/eycloud')
+    list.tasks.create(:name => 'We blog http://www.engineyard.com/blog/')
+    list.tasks.create(:name => 'Rock on!')
   end
 
   def self.down
