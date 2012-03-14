edf4aba4c629e27ea36a5b12fe28e5eb37ddde31
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-13 23:00:26 -0700

    revert to 'all applications get delayed job recipe'

--- deploy/cookbooks/delayed_job/libraries/delayed_job_applications.rb
+++ deploy/cookbooks/delayed_job/libraries/delayed_job_applications.rb
@@ -2,14 +2,7 @@ class Chef
   class Recipe
     # Only Rails applications (containing script/runner) are valid
     def delayed_job_applications
-      node[:applications].inject([]) do |apps, app_name_and_data|
-        app_name, _ = app_name_and_data
-        app_path = "/data/#{app_name}/current/script/runner"
-        if File.exist?(app_path)
-          apps << app_name_and_data
-        end
-        apps
-      end
+      node[:applications]
     end
   end
 end
