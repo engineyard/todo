8f72f0e1314d478b850e96e2dab0e18dc55364c2
Jacob Burkhart <jburkhart@engineyard.com>
2012-04-19 14:10:28 -0700

    try this, try that

--- deploy/solo.rb
+++ deploy/solo.rb
@@ -1,4 +1,5 @@
 puts "running solo..."
+node = Yajl::Parser.new.parse(File.read("/etc/chef/dna.json"))
 puts "node: " + node.inspect
 
 base_dir = File.dirname(__FILE__)
@@ -12,17 +13,3 @@ role_path         "#{base_dir}/roles"
 node_name         node["engineyard"]["this"]
 
 puts "node: " + node.inspect
-
-require "fileutils"
-$LOAD_PATH.each do |path|
-  recipe_path = path.gsub(%r{/lib$},'')
-  if File.exist?(metadata_path = File.join(recipe_path, 'metadata.json'))
-    metadata = Yajl::Parser.new.parse(File.new(metadata_path, "r"))
-    if metadata["name"]
-      puts "Unpacking #{metadata["name"]} recipe into cookbooks from #{cookbooks_path}"
-      FileUtils.cp_r(recipe_path, File.join(cookbooks_path, metadata["name"]))
-    else
-      puts "ERROR: Recipe has no 'name' in metadata.json (#{cookbooks_path})"
-    end
-  end
-end
