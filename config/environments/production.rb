0aa651eb2bcbb8dbf4900ce7e0b516cafb634126
Jacob Burkhart <jburkhart@engineyard.com>
2014-04-10 16:02:41 -0700

    put the asset host back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Listr::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
-  config.action_controller.asset_host = "is-my-cdn-working-engineyard-com.global.ssl.fastly.net"
+  config.action_controller.asset_host = EY::Config.get(:fastly, "FASTLY_CDN_URL")
   config.static_cache_control = "public, max-age=2592000"
   config.assets.digest = true
 
