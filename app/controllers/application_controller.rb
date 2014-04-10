601d0d0b09790900236e82cd58aceca193b8bfcb
Jacob Burkhart <jburkhart@engineyard.com>
2014-04-10 16:30:21 -0700

    cache 1 second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_cache_control_headers
 
-  def set_cache_control_headers(max_age = 10.seconds.to_s)
+  def set_cache_control_headers(max_age = 1.seconds.to_s)
     if request.method == "GET"
       request.session_options[:skip] = true  # removes session data
       response.headers['Cache-Control'] = 'public, no-cache'
+      # response.headers['Surrogate-Key'] = "blah"
       response.headers['Surrogate-Control'] = "max-age: #{max_age}"
     end
   end
