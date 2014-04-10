e9b727055272e996b5cddbece0445af785592f6a
Jacob Burkhart <jburkhart@engineyard.com>
2014-04-10 15:44:38 -0700

    precompile it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Listr::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( test.css )
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
