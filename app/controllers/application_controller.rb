2b7245957ace8045a23da5bb4dc2e93f18ae327a
Jacob Burkhart <jburkhart@engineyard.com>
2014-04-11 16:47:18 -0700

    cache 1 second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_cache_control_headers
+  before_filter :cache_or_purge
 
-  def set_cache_control_headers(max_age = 1.minute.to_s)
+  def cache_or_purge(max_age = 1.second.to_s)
     if request.method == "GET"
       request.session_options[:skip] = true  # removes session data
       response.headers['Cache-Control'] = 'public, no-cache'
       response.headers['Surrogate-Key'] = "blah"
       response.headers['Surrogate-Control'] = "max-age=#{max_age}"
+    # else
+    #   api_key = EY::Config.get(:fastly, "FASTLY_API_KEY")
+    #   service_id = EY::Config.get(:fastly, "FASTLY_SERVICE_ID")
+    #   Excon.new('https://api.fastly.com').post(
+    #     :headers => {'Fastly-Key' => api_key},
+    #     :path => "/service/#{service_id}/purge/blah")
     end
   end
 
