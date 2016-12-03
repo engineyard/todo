80e9a7e63a9aa914ba8fa93575ff484ab55ff0f6
Allan Espinosa <aespinosa@engineyard.com>
2016-12-03 11:17:03 +0900

    Run the hook on solo

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,3 +1,10 @@
+on_app_master do
+  if config.current_role == 'solo'
+    run "[[ -d #{config.shared_path}/sphinx ]] && ln -nfs #{config.shared_path}/sphinx #{config.current_path}/db/sphinx"
+    run "cd #{config.current_path} && RAILS_ENV=#{config.environment} bundle exec rake ts:configure"
+  end
+end
+
 on_utilities('sphinx') do
   run "[[ -d #{config.shared_path}/sphinx ]] && ln -nfs #{config.shared_path}/sphinx #{config.current_path}/db/sphinx"
   run "cd #{config.current_path} && RAILS_ENV=#{config.environment} bundle exec rake ts:configure"
