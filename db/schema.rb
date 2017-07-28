59b440bc7c6e2126f2822dc44f9d8510825a1c45
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2017-07-28 11:00:14 +0300

    Add page to display environment variables.

--- db/schema.rb
+++ db/schema.rb
@@ -9,21 +9,21 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110613204114) do
+ActiveRecord::Schema.define(version: 20110613204114) do
 
-  create_table "lists", :force => true do |t|
+  create_table "lists", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "tasks", :force => true do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "done"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "list_id"
   end
 
