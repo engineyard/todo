da2d00edc709902f3707d5498db847499f935425
Radamanthus Batnag <radamanthus@gmail.com>
2017-10-25 11:14:15 +0800

    More deploy hook tests

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,3 +3,7 @@ warning "Hello there, STDERR!"
 debug "Debug here, checking in!"
 
 run "echo 'Hello from run, STDERR!' 1>&2"
+
+on_utilities :elasticsearch do
+  warning "This will not run - use strings instead of symbols, nitwit"
+end
