37723c721ce8002eaeb5f9f03191c155c11d3c24
Pasan Chamikara <gwpc92@gmail.com>
2022-10-28 21:52:03 +0530

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Listr::Application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.assets.enabled = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
